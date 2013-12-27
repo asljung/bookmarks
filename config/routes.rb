60812743c58786efe9ed631021108c8b1524a494
Daniel Åsljung <daniel.asljung@gmail.com>
2013-12-27 13:59:37 +0100

    Installed and configured  Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Bookmarks::Application.routes.draw do
+  resources :bookmarks
+  root 'bookmarks#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
