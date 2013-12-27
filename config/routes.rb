032c832bb640a11598475e7958e2a67f1bb00a99
Daniel Åsljung <daniel.asljung@gmail.com>
2013-12-27 14:27:28 +0100

    Bookmarks connected to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Bookmarks::Application.routes.draw do
   devise_for :users
   resources :bookmarks
   root 'bookmarks#index'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
